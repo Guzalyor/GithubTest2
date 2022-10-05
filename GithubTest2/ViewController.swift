a10f7445c7053f2fa99187c7e9b53f0eda81cc26
Guzalyor <guzalyorikromova@gmail.com>
2022-10-05 10:44:17 +0200

    Initial Commit

---
+++ GithubTest2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubTest2
+//
+//  Created by Guzalyor Ikromova on 05/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
